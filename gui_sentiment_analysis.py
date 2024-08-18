95ad52860100751fc474941210be8186307fda4a
Nguyen Thi My Huyen <93422352+HuyenNguyen07@users.noreply.github.com>
2024-08-18 18:07:50 +0700

    Update gui_sentiment_analysis.py

--- gui_sentiment_analysis.py
+++ gui_sentiment_analysis.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from geopy.geocoders import Nominatim
-import folium
-from streamlit_folium import st_folium
 
 import seaborn as sns
 import matplotlib.pyplot as plt
