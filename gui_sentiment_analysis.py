a2886b531d0a45f7f5c97b3b8900c74de216ea38
Nguyen Thi My Huyen <93422352+HuyenNguyen07@users.noreply.github.com>
2024-08-22 00:33:32 +0700

    Update gui_sentiment_analysis.py

--- gui_sentiment_analysis.py
+++ gui_sentiment_analysis.py
@@ -195,14 +195,14 @@ def pre_process(comment,emoji_dict, english_dict, teen_dict, wrong_lst,stopwords
 
 vectorizer = TfidfVectorizer()
 
-# Đọc vectorizer
-# with open(vectorizer.pkl, 'rb') as file:  
-#     vectorizer = pickle.load(file)
+Đọc vectorizer
+with open("vectorizer.pkl", 'rb') as file:  
+    vectorizer = pickle.load(file)
 
 
-# Đọc model
-# with open("sentiment_analysis.pkl", 'rb') as file:  
-#     sa_model = pickle.load(file)
+Đọc model
+with open("sa_model.pkl", 'rb') as file:  
+    sa_model = pickle.load(file)
 
 
 # # Hàm lấy lat, lon
